3e574717503d29d1a7ba90ac8e049266e0081703
AmanChaudhary1998 <chaudharyaman50@yahoo.com>
2020-10-19 17:59:45 +0530

    task_under_construction

---
+++ src/cats/cats.resolver.ts
@@ -0,0 +1,33 @@
+import { Resolver,Query, Mutation, Args } from "@nestjs/graphql";
+import { CatsService } from "./cats.service";
+import { CatType } from "./dto/create-cat.dto";
+import { CatInput } from "./inputs/cat.input";
+
+@Resolver()
+export class CatsResolver {
+  constructor(
+    private readonly catsService: CatsService, 
+  ) {}
+
+  @Query(()=> String)
+  async hello() {
+    return "Hello";
+  }
+
+  @Query(()=> [CatType])
+  async cats() {
+    return this.catsService.findAll();
+  }
+
+  @Mutation(()=> CatType)
+  async createCat(@Args('input')  input : CatInput) {
+    return this.catsService.create(input);
+  }
+   
+   
+//   @ResolveField()
+//   async posts(@Parent() author: Author) {
+//     const { id } = author;
+//     return this.postsService.findAll({ authorId: id });
+//   }
+}
